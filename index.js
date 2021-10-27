b5f29e8672c02e100103541b5adf77475fa79a94
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-10-27 15:00:34 +0000

    Finished challenges

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(number){
+  if (number >= 5 && number <= 10){
+      return true
+  }else{
+      return false
+  }
 }
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(number){
+   if (number >= 10 && number <= 30){
+      return true
+  }else{
+      return false
+  }
 }
 
 //3
-function betweenThirtyAndFifty(){
-  
+function betweenThirtyAndFifty(number){
+   if (number >= 30 && number <= 50){
+      return true 
+  }else{
+      return false
+  }
 }
 
 
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(number){
+   if (number >= 0 && number <= 30 || number >= 50 && number <= 70 ){
+      return true
+  }else{
+      return false
+  }
 }
 
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(number){
+   if (number >= 20 && number <= 50 && number <= 60){
+      return true
+  }else{
+      return false
+  }
 }
 
 //6
